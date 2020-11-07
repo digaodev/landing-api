dd96653be4b62ba7d9429f9565d1257ff16f39ae
Rodrigo RB <rodrigo.rb.dev@gmail.com>
2020-11-07 19:56:11 -0300

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '48393ad61fd9bdfdc991083a1bf87f31'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'ef006046-ca1d-4835-8567-c7e1814359a2'
+};
