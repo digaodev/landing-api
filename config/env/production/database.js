e6ff1902ae0af5d9431797948a591d825c544aec
Rodrigo RB <rodrigo.rb.dev@gmail.com>
2020-12-05 16:28:45 -0300

    chore: test heroku config

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,26 +1,28 @@
 const parse = require('pg-connection-string').parse;
 const config = parse(process.env.DATABASE_URL)
 
-console.log('=========')
-console.log(env('AWS_ACCESS_KEY_ID'), env('AWS_ACCESS_SECRET'), env('AWS_REGION'), env('AWS_BUCKET'))
-console.log('=========')
+module.exports = ({ env }) => {
+  console.log('=========')
+  console.log(env('AWS_ACCESS_KEY_ID'), env('AWS_ACCESS_SECRET'), env('AWS_REGION'), env('AWS_BUCKET'))
+  console.log('=========')
 
-module.exports = ({ env }) => ({
-  defaultConnection: 'default',
-  connections: {
-    default: {
-      connector: 'bookshelf',
-      settings: {
-        client: 'postgres',
-        host: config.host,
-        port: config.port,
-        database: config.database,
-        username: config.user,
-        password: config.password,
+  return ({
+    defaultConnection: 'default',
+    connections: {
+      default: {
+        connector: 'bookshelf',
+        settings: {
+          client: 'postgres',
+          host: config.host,
+          port: config.port,
+          database: config.database,
+          username: config.user,
+          password: config.password,
+        },
+        options: {
+          ssl: false
+        }
       },
-      options: {
-        ssl: false
-      }
     },
-  },
-});
+  });
+}
